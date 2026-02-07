15754c2b2f9f657c8e7988bf70d8ddafaf162a5f
Prince James Nagar <prince1.230101094@iiitbh.ac.in>
2026-02-07 16:04:34 +0530

    cal init

---
+++ src/index.ts
@@ -0,0 +1,40 @@
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (_, res) => {
+    res.send('Calculator Home Page');
+})
+
+app.get('/sum/:a/:b', (req, res) => {
+    const a:number = Number(req.params.a);
+    const b:number = Number(req.params.b);
+    res.json({result: a + b});
+})
+app.get('/diff/:a/:b', (req, res) => {
+    const a:number = Number(req.params.a);
+    const b:number = Number(req.params.b);
+    res.json({result: a - b});
+})
+app.get('/mul/:a/:b', (req, res) => {
+    const a:number = Number(req.params.a);
+    const b:number = Number(req.params.b);
+    res.json({result: a * b});
+})
+app.get('/div/:a/:b', (req, res) => {
+    const a:number = Number(req.params.a);
+    const b:number = Number(req.params.b);
+    if(b == 0) {
+        res.json({result: 'Cannot divide by 0'});
+    }
+    else {
+        res.json({result: a / b});
+    }
+})
+
+app.listen(PORT, () => console.log('calculator server started at PORT : ', PORT));
